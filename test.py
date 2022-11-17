0d6ea420f63825f8248ae3a36015fd561c2277a4
ciantfv456 <57960695+ciantfv456@users.noreply.github.com>
2022-11-17 13:32:07 +0200

    Update test.py

--- test.py
+++ test.py
@@ -4,5 +4,5 @@ from selenium.webdriver.common.keys import Keys
 driver = webdriver.Chrome('./chromedriver')
 
 driver.get("http://localhost:3000")
-
+print(driver.title)
 assert driver.title == 'Express'
